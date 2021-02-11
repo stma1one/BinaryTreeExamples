0971acd49f00a4d9effd071bcb671aa68d2dec5b
Home <Home@LAPTOP-2E6V6ACM>
2021-02-11 11:26:20 +0200

    Add project files.

---
+++ BinaryTreeExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using DataStructure;
+namespace BinaryTreeExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
